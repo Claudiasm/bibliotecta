6e2382356e3f0e4269cd69f9c1a860e93f0598fd
Claudiasm <92579338+Claudiasm@users.noreply.github.com>
2022-03-01 13:22:34 +0000

    Add files via upload

---
+++ usuarios/modificar.php
@@ -0,0 +1,91 @@
+<?php
+
+require "../configuracion/configuracion.php";
+require "../helpers/funciones.php";
+
+
+
+// Variables
+$id_usuario = isset($_REQUEST['id_usuario']) ? $_REQUEST['id_usuario'] : null;
+$nombre     = isset($_REQUEST['nombre']) ? $_REQUEST['nombre'] : null;
+$apellidos  = isset($_REQUEST['apellidos']) ? $_REQUEST['apellidos'] : null;
+$email      = isset($_REQUEST['email']) ? $_REQUEST['email'] : null;
+$password   = isset($_REQUEST['password']) ? $_REQUEST['password'] : null;
+$tipo       = isset($_REQUEST['tipo']) ? $_REQUEST['tipo'] : null;
+
+
+tienePermisos($id_usuario);
+
+// Comprobamos si recibimos datos por POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $imagen = $_FILES['imagen']['name'];
+    $type   = $_FILES['imagen']['type'];
+    $tamano = $_FILES['imagen']['size'];
+    echo $imagen;
+
+    if (!empty($imagen) && ($_FILES['imagen']['size'] <= 200000000)) {
+        if (($_FILES["imagen"]["type"] == "image/gif")
+            || ($_FILES["imagen"]["type"] == "image/jpeg")
+            || ($_FILES["imagen"]["type"] == "image/jpg")
+            || ($_FILES["imagen"]["type"] == "image/png"))
+        {
+            $directorio = 'images/';
+            move_uploaded_file($_FILES['imagen']['tmp_name'],$directorio.$imagen);
+        }
+        else
+        {
+            echo "No se puede subir una imagen con ese formato ";
+        }
+    } else {
+        if($imagen == !NULL) echo "La imagen es demasiado grande ";
+    }
+
+    $sentencia = 'UPDATE usuarios 
+        SET nombre = :nombre, apellidos = :apellidos, email = :email, '. 
+        (!empty($password) ? 'password = :password, ' : '') .
+        (!empty($imagen) ? 'imagen = :imagen, ' : '') .
+        'tipo = :tipo ' . 
+        'WHERE id_usuario = :id_usuario';
+    // Prepara UPDATE
+    $miUpdate = $miPDO->prepare($sentencia);
+
+    $parametros = [
+        'id_usuario' => $id_usuario,
+        'nombre'     => $nombre,
+        'apellidos'  => $apellidos,
+        'email'      => $email,
+        'tipo'       => $tipo
+    ];
+
+    if (!empty($imagen) ) {
+        $parametros['imagen'] = $imagen;
+    }
+    
+    if(!empty($password)){
+        $passwordEncryt=password_hash($password, PASSWORD_DEFAULT);
+        $parametros['password'] = $passwordEncryt;
+    }
+
+    // Ejecuta UPDATE con los datos
+    $miUpdate->execute($parametros);
+
+    $_SESSION["mensajes"] = "Se ha modificado corectamente";
+    // Redireccionamos a Leer
+    header('Location: index.php');
+} else {
+    $miConsulta = $miPDO->prepare('SELECT * FROM usuarios WHERE id_usuario = :id_usuario');
+    // Ejecuta la sentencia SQL
+    $miConsulta->execute([
+        'id_usuario' => $id_usuario
+    ]);
+    // Obtiene un resultado
+    $datos = $miConsulta->fetch();
+
+    echo $blade -> run("usuarios.modificar", [
+        'datos' => $datos
+]   );
+}
+
+
+?>
